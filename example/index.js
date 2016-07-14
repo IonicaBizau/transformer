26a4948850041159f0d9c0f1c637694d14667eb0
Ionică Bizău <bizauionica@gmail.com>
2016-07-14 15:38:54 +0300

    Working prototype

--- example/index.js
+++ example/index.js
@@ -1,5 +1,64 @@
 "use strict";
 
-const transformer = require("../lib");
+const Transformer = require("../lib");
 
-console.log(transformer());
+let t = new Transformer({ world: "Earth" });
+
+
+// There are three levels where the functions are added to be executed:
+//
+// Parallel:               | <0: [.............................................]>
+// Unordered (don't wait): |                                <4a: [........]>
+//                         +                                <4b: [....]>
+//                         +                                <4c: [......]>
+// Ordered (wait):         | <1: [...]> <2: [.]> <3:[.....]>                <5: [....]>
+
+// This will run in parallel with all the other functions
+t.add((data, cb) => {
+    setTimeout(() => {
+        data.parallel = 42;
+        cb();
+    }, 2000);
+}, Transformer.PARALLEL);
+
+// Async function, but ordered
+t.add((data, cb) => {
+    setTimeout(() => {
+        data.oldWorld = data.world;
+        data.world = "Mars";
+        cb();
+    }, 1000);
+});
+
+// Another async function and ordered
+t.add((data, cb) => {
+    setTimeout(() => {
+        data.baz = 7;
+        cb();
+    }, 500);
+});
+
+// Async function, but not ordered
+t.add((data, cb) => {
+    setTimeout(() => {
+        data.foo = 42;
+        cb();
+    }, 1000);
+}, Transformer.UNORDERED);
+
+// Another unordered function (this will end sooner)
+t.add((data, cb) => {
+    setTimeout(() => {
+        data.bar = 42;
+        cb(null, data);
+    }, 900);
+}, Transformer.UNORDERED);
+
+debugger
+// Sync function
+t.add(data => {
+    data.planet = data.world;
+});
+
+// Finally show the data
+t.on("end", data => console.log(data));
